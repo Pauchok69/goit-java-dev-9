b8916cdebe0a42f89cab80b7fab2afafe8c8d772
andy <pauchokbk@gmail.com>
2023-03-21 17:56:10 +0200

    Add mechanism to save custom timezone to Cookie and get it from Cookie if no timezone parameters were send

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -2,6 +2,7 @@ package org.example;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.IOException;
+import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,6 +22,7 @@ public class TimeServlet extends HttpServlet {
     public static final String DEFAULT_TIME_ZONE = "UTC";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd hh-mm-ss VV";
     public static final String QUERY_PARAM_TIMEZONE = "timezone";
+    public static final String LAST_TIMEZONE_COOKIE = "lastTimezone";
     private TemplateEngine engine;
 
     @Override
@@ -37,8 +40,7 @@ public class TimeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Optional<String> timezoneOptional = Optional.ofNullable(req.getParameter(QUERY_PARAM_TIMEZONE));
-        ZoneId zoneId = ZoneId.of(timezoneOptional.orElse(DEFAULT_TIME_ZONE));
+        ZoneId zoneId = defineZoneId(req, resp);
         ZonedDateTime now = ZonedDateTime.now(zoneId);
 
         resp.setContentType("text/html");
@@ -48,5 +50,49 @@ public class TimeServlet extends HttpServlet {
 
         engine.process("time", simpleContext, resp.getWriter());
         resp.getWriter().close();
+    }
+
+    private static ZoneId defineZoneId(HttpServletRequest req, HttpServletResponse resp) {
+        Optional<String> timeZoneFromRequest = getTimeZoneFromRequest(req);
+
+        if (timeZoneFromRequest.isPresent()) {
+            String timezone = timeZoneFromRequest.get();
+            resp.addCookie(new Cookie(LAST_TIMEZONE_COOKIE, timezone));
+
+            return ZoneId.of(timezone);
+        }
+        Optional<String> timeZoneFromCookie = getTimeZoneFromCookie(req);
+
+        if (timeZoneFromCookie.isPresent()) {
+            String timezone = timeZoneFromCookie.get();
+
+            try {
+                return ZoneId.of(timezone);
+            } catch (DateTimeException ex) {
+                return ZoneId.of(DEFAULT_TIME_ZONE);
+            }
+        }
+
+        return ZoneId.of(DEFAULT_TIME_ZONE);
+    }
+
+    private static Optional<String> getTimeZoneFromRequest(HttpServletRequest req) {
+        return Optional.ofNullable(req.getParameter(QUERY_PARAM_TIMEZONE));
+    }
+
+    private static Optional<String> getTimeZoneFromCookie(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+
+        if (cookies == null) {
+            return Optional.empty();
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(LAST_TIMEZONE_COOKIE)) {
+                return Optional.ofNullable(cookie.getValue());
+            }
+        }
+
+        return Optional.empty();
     }
 }
